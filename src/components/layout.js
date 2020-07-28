857eda8e21926154b7818b0605664d7b9a63b461
Stephen Belyea <sbelyea@healthhubsolutions.ca>
2020-07-27 21:45:51 -0400

    Shift stylesheet to static dir and call from helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby'
 
+const year = (new Date()).getFullYear();
+const rootPath = `${__PATH_PREFIX__}/`;
+
 function Layout({ location, title, children }) {
   const { site } = useStaticQuery(
     graphql`
@@ -17,7 +20,6 @@ function Layout({ location, title, children }) {
     `
   );
 
-  const rootPath = `${__PATH_PREFIX__}/`;
   const titleTemplate = location.pathname === rootPath
     ? site.siteMetadata.title
     : `%s | ${site.siteMetadata.title}`;
@@ -32,27 +34,26 @@ function Layout({ location, title, children }) {
         }}
       >
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:700|Crimson+Text:400,700"></link>
+        <link rel="stylesheet" href="/styles.css" />
       </Helmet>
       <div className="layout">
         <div className="info">
           <p>Site info</p>
         </div>
         <main className="main">
-          <p>Page content</p>
+          <h1>{title}</h1>
+          {children}
         </main>
         <div className="feed">
           <p>Feed links</p>
         </div>
-        {/* <header>
-          <h1>{title}</h1>
-        </header>
-        <main>
-          {children}
-        </main>
-        <footer>
-          <p>&copy;</p>
-        </footer> */}
       </div>
+      <footer>
+        <p className="text-centre">
+          &copy;{year} <strong><span className="red">bel</span>werks</strong> | Stephen Belyea
+        </p>
+      </footer>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import { Layout } from '../components';
-import '../styles.css';
 
 function BlogIndex({ location, data }) {
-  console.log('data', data)
 
-  const { siteMetadata } = data.site;
   return (
-    <Layout title="Blog" location={location}>
-      <p>hello there</p>
+    <Layout title="Belwerks Blog" location={location}>
+      <p>Hello there</p>
     </Layout>
   )
 }
 
 export default BlogIndex;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
